c09de85e55b2772d85b7c45446acacd4ca57ce04
Antoine Zampunieris <etu52878@henallux.be>
2024-05-06 11:54:50 +0200

    test mdf

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,10 @@ int myFunction(int x, int y) {
 
 #include <QTRSensors.h>
 
+#include <DRV8835MotorShield.h>
+
+//#define LED_PIN 13
+
 //#include <stdint.h>
 
 // This example is designed for use with eight RC QTR sensors. These
@@ -47,9 +51,10 @@ int myFunction(int x, int y) {
 // 0 before being lost. 5000 means the line is directly under sensor 5 or was
 // last seen by sensor 5 before being lost.
 
-//QTRSensors qtr;
-/*
-//QTRSensorsRC qtrrc((unsigned char[]) {0, 9, 15, 17}, 4);
+QTRSensors qtr;
+DRV8835MotorShield motors;
+
+//QTRSensorsRC qtrrc((unsigned char[]) {3, 4, 5, 6, 7, 8, 9, 10}, 8);
 const uint8_t SensorCount = 8;
 uint16_t sensorValues[SensorCount];
 
@@ -58,15 +63,15 @@ void setup()
 
   //pins de l'Arduino 
 
-  pinMode(2, OUTPUT);
-  pinMode(3, INPUT);
+  /*pinMode(2, OUTPUT);
+  /*pinMode(3, INPUT);
   pinMode(4, INPUT);
   pinMode(5, INPUT);
   pinMode(6, INPUT);
   pinMode(7, INPUT);
   pinMode(8, INPUT);
   pinMode(9, INPUT);
-  pinMode(10, INPUT);
+  pinMode(10, INPUT);*/
 
 
   // configure the sensors
@@ -75,6 +80,8 @@ void setup()
   qtr.setEmitterPin(2);
 
   delay(500);
+  digitalWrite(2,HIGH);
+ 
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
 
@@ -105,6 +112,18 @@ void setup()
   Serial.println();
   Serial.println();
   delay(1000);
+  demarrage();
+}
+
+void demarrage(){
+  for (int speed = 0; speed <= 400; speed++)
+  {
+    motors.setM1Speed(speed);
+    motors.setM2Speed(speed);
+    delay(2);
+  }
+  
+
 }
 
 void loop()
@@ -124,7 +143,29 @@ void loop()
   Serial.println(position);
 
   delay(250);
-}*/
+  
+  /*if (sensorValues[3]>900||sensorValues[4]>900){
+
+
+
+  }
+  if(sensorValues[0]>900||sensorValues[1]>900||sensorValues[2]>900){
+
+  }
+
+
+  for (int speed = 0; speed <= 400; speed++)
+  {
+    motors.setM1Speed(speed);
+    delay(2);
+  }
+
+  for (int speed = 400; speed >= 0; speed--)
+  {
+    motors.setM1Speed(speed);
+    delay(2);
+  }*/
+}
 
 // This example is designed for use with eight RC QTR sensors. These
 // reflectance sensors should be connected to digital pins 3 to 10. The
@@ -140,6 +181,7 @@ void loop()
 // setTimeout()).
 
 /*QTRSensors qtr;
+DRV8835MotorShield motors;
 
 const uint8_t SensorCount = 8;
 uint16_t sensorValues[SensorCount];
@@ -165,6 +207,12 @@ void setup()
   digitalWrite(2,HIGH);
 
   Serial.begin(9600);
+
+  pinMode(LED_PIN, OUTPUT);
+  
+  // uncomment one or both of the following lines if your motors' directions need to be flipped
+  //motors.flipM1(true);
+  //motors.flipM2(true);
 }
 
 
@@ -215,98 +263,97 @@ void loop()
   Serial.println();
 
   delay(250);
+}
+
+
+//ici
+//#include <DRV8835MotorShield.h>
+
+/*
+ * This example uses the DRV8835MotorShield library to drive each motor with the
+ * Pololu DRV8835 Dual Motor Driver Shield for Arduino forward, then backward. 
+ * The yellow user LED is on when a motor is set to a positive speed and off when
+ * a motor is set to a negative speed.
+ */
+
+//#define LED_PIN 13
+
+//DRV8835MotorShield motors;
+
+/*void setup()
+{
+  pinMode(LED_PIN, OUTPUT);
+  
+  // uncomment one or both of the following lines if your motors' directions need to be flipped
+  //motors.flipM1(true);
+  //motors.flipM2(true);
 }*/
-//#include <QTRSensors.h>
 
-// This example is designed for use with six analog QTR sensors. These
-// reflectance sensors should be connected to analog pins A0 to A5. The
-// sensors' emitter control pin (CTRL or LEDON) can optionally be connected to
-// digital pin 2, or you can leave it disconnected and remove the call to
-// setEmitterPin().
-//
-// The setup phase of this example calibrates the sensors for ten seconds and
-// turns on the Arduino's LED (usually on pin 13) while calibration is going
-// on. During this phase, you should expose each reflectance sensor to the
-// lightest and darkest readings they will encounter. For example, if you are
-// making a line follower, you should slide the sensors across the line during
-// the calibration phase so that each sensor can get a reading of how dark the
-// line is and how light the ground is. Improper calibration will result in
-// poor readings.
-//
-// The main loop of the example reads the calibrated sensor values and uses
-// them to estimate the position of a line. You can test this by taping a piece
-// of 3/4" black electrical tape to a piece of white paper and sliding the
-// sensor across it. It prints the sensor values to the serial monitor as
-// numbers from 0 (maximum reflectance) to 1000 (minimum reflectance) followed
-// by the estimated location of the line as a number from 0 to 5000. 1000 means
-// the line is directly under sensor 1, 2000 means directly under sensor 2,
-// etc. 0 means the line is directly under sensor 0 or was last seen by sensor
-// 0 before being lost. 5000 means the line is directly under sensor 5 or was
-// last seen by sensor 5 before being lost.
-
-QTRSensors qtr;
-
-const uint8_t SensorCount = 6;
-uint16_t sensorValues[SensorCount];
-
-void setup()
+/*void loop()
 {
-  // configure the sensors
-  qtr.setTypeAnalog();
-  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5}, SensorCount);
-  qtr.setEmitterPin(2);
+  // run M1 motor with positive speed
+  
+  digitalWrite(LED_PIN, HIGH);
+  
+  for (int speed = 0; speed <= 400; speed++)
+  {
+    motors.setM1Speed(speed);
+    delay(2);
+  }
 
+  for (int speed = 400; speed >= 0; speed--)
+  {
+    motors.setM1Speed(speed);
+    delay(2);
+  }
+  
+  // run M1 motor with negative speed
+  
+  /*digitalWrite(LED_PIN, LOW);
+  
+  for (int speed = 0; speed >= -400; speed--)
+  {
+    motors.setM1Speed(speed);
+    delay(2);
+  }
+  
+  for (int speed = -400; speed <= 0; speed++)
+  {
+    motors.setM1Speed(speed);
+    delay(2);
+  }
+
+  // run M2 motor with positive speed
+  
+  /*digitalWrite(LED_PIN, HIGH);
+  
+  for (int speed = 0; speed <= 400; speed++)
+  {
+    motors.setM2Speed(speed);
+    delay(2);
+  }
+
+  for (int speed = 400; speed >= 0; speed--)
+  {
+    motors.setM2Speed(speed);
+    delay(2);
+  }
+  
+  // run M2 motor with negative speed
+  
+  digitalWrite(LED_PIN, LOW);
+  
+  for (int speed = 0; speed >= -400; speed--)
+  {
+    motors.setM2Speed(speed);
+    delay(2);
+  }
+  
+  for (int speed = -400; speed <= 0; speed++)
+  {
+    motors.setM2Speed(speed);
+    delay(2);
+  }
+  
   delay(500);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
-
-  // analogRead() takes about 0.1 ms on an AVR.
-  // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
-  // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
-  // Call calibrate() 400 times to make calibration take about 10 seconds.
-  for (uint16_t i = 0; i < 400; i++)
-  {
-    qtr.calibrate();
-  }
-  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
-
-  // print the calibration minimum values measured when emitters were on
-  Serial.begin(9600);
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.minimum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-
-  // print the calibration maximum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.maximum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
-  delay(1000);
-}
-
-void loop()
-{
-  // read calibrated sensor values and obtain a measure of the line position
-  // from 0 to 5000 (for a white line, use readLineWhite() instead)
-  uint16_t position = qtr.readLineBlack(sensorValues);
-
-  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
-  // reflectance and 1000 means minimum reflectance, followed by the line
-  // position
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(sensorValues[i]);
-    Serial.print('\t');
-  }
-  Serial.println(position);
-
-  delay(250);
-}
-
-
+}*/
